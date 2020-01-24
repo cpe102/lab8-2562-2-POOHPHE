4cc8081cdc314f02d9fb8e5a804a6a544bcf27dd
FALLENSLAYER007 <37149762+POOHPHE@users.noreply.github.com>
2020-01-24 15:17:27 +0700

    Add files via upload
    Lab 8 24/1/2020

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,10 +3,18 @@
 using namespace std;
 
 int main(){	
-
+	long double loan,rate,mon,cur_ba,cur_int,total;
+	cout<<"Enter initial loan: ";
+	cin>>loan;
+	cout<<"Enter interest rate per year (%): ";
+	cin>>rate;
+	cout<<"Enter iamount you can pay per year: ";
+	cin>>mon;
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
+	
+	
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -15,16 +23,32 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
+	
+	long long i=1;
+	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
+	do{
+	cur_int=loan*rate/100.0;
+	total=loan+cur_int;
+	cur_ba=total-mon;
+	
+	if(total<mon){
+		mon=total;
+		cur_ba=0;
+	}
+	
+	cout << fixed << setprecision(2); 
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << cur_int;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << mon;
+	cout << setw(13) << left << cur_ba;
+	cout << "\n";	
+	loan=cur_ba;
+	i++;
+	}while(loan!=0);
 	return 0;
 }
--- lab8_2.cpp
+++ lab8_2.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(long a,long b){
+	if(a>0&&b>0){
+		
+	
+	for(int i=0;i<a;i++){
+	for(int j=0;j<b;j++){
+		cout<<"0";
+	}	cout<<endl;
+	}
+	}else{
+		cout<<"Invalid input";
+	}
+}
+
+
 
 int main(){
 	
